fa8ce3cce4f9ed657f89b898c0498212dc4603e4
Bermet <oceaninapuddle@gmail.com>
2023-01-29 17:53:40 +0600

    fixed Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { MenuItems } from "./MenuItems";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked((prev) => !prev);
+  };
+
   return (
     <nav className="NavbarItems">
       <h1 className="navbar-logo">Trips</h1>
+
+      <div className="menu-icons" onClick={handleClick}>
+        <i className={clicked ? "fas fa-times" : "fas fa-bars"}></i>
+      </div>
 
       <ul className="nav-menu">
         {MenuItems.map((item, index) => {
