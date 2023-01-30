79d179ecbc54f85d787e7f21fdc6f72e37518347
Bermet <oceaninapuddle@gmail.com>
2023-01-30 23:07:35 +0600

    fixed component Servise

--- src/routes/Service.js
+++ src/routes/Service.js
@@ -3,11 +3,13 @@ import React from "react";
 import Hero from "../components/Hero";
 
 import night from "../assets/night.jpg";
+import Trips from "../components/Trips";
 
 const Service = () => {
   return (
     <>
       <Hero cName="hero-mid" heroImg={night} title="Service" btnClass="hide" />
+      <Trips />
     </>
   );
 };
